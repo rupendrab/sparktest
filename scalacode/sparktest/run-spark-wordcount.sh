13164a5e825ba411daf54f7e38552be4f009a87d
Rupendra Bandyopadhyay <rbandyo@yahoo.com>
2017-03-16 13:52:02 +0000

    Modified WordCount to add local as master in SparkConf

--- scalacode/sparktest/run-spark-wordcount.sh
+++ scalacode/sparktest/run-spark-wordcount.sh
@@ -2,4 +2,7 @@
 
 bindir=$(cd $(dirname $0); pwd)
 cd ${bindir}
+
+export SPARK_HOME=/data/spark-2.1.0-bin-hadoop2.6
+
 $SPARK_HOME/bin/spark-submit --class example.WordCount ./target/scala-2.11/sparktest_2.11-0.1.0.jar ./github.txt 2>console.log
--- scalacode/sparktest/src/main/scala/example/WordCount.scala
+++ scalacode/sparktest/src/main/scala/example/WordCount.scala
@@ -7,7 +7,7 @@ object WordCount {
 
     def main(args: Array[String]) {
         val inputFile = args(0)
-        val conf = new SparkConf().setAppName("wordCount")
+        val conf = new SparkConf().setMaster("local[2]").setAppName("wordCount")
         val sc = new SparkContext(conf)
         val input = sc.textFile(inputFile)
         val words = input.flatMap(line => line.split(" "))
